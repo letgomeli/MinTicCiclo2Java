7c634e046e51e2815e6f6401930260e8a9c83637
Friné Coral Velásquez <86983043+letgomeli@users.noreply.github.com>
2021-08-23 11:24:31 -0500

    Add files via upload

---
+++ Reto1Java-master/CompararRentabilidadCasos/src/CompuService.java
@@ -0,0 +1,11 @@
+public class CompuService {
+	public static void main(String[] args) {
+		System.out.println("Caso de prueba 1");
+		CompararRentabilidadCasos comparar1 = new CompararRentabilidadCasos(12, 2000000, 5.5);
+		System.out.println(comparar1.compararRentabilidad());
+
+		System.out.println("Caso de prueba 2");
+		CompararRentabilidadCasos comparar2 = new CompararRentabilidadCasos(12,20000000,5);
+		System.out.println(comparar2.compararRentabilidad());
+	} 
+}

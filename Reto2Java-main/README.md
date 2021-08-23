da65acae8dc6709b0c8f43a10ac7ce62ec752713
Friné Coral Velásquez <86983043+letgomeli@users.noreply.github.com>
2021-08-23 11:27:41 -0500

    Add files via upload

---
+++ Reto2Java-main/README.md
@@ -0,0 +1,2 @@
+# Reto2Java
+Desarrollo del Reto2 del Ciclo2 de Misión 2022
---
+++ Reto2Java-main/Reto2/src/Vehiculo.java
@@ -0,0 +1,41 @@
+class Vehiculo {
+  private final double precioBase;
+	private final boolean lujo;
+	private String marca;
+	private int modelo;
+	private double precioFinal;
+	private int nroLlantas;
+
+	public Vehiculo(double precioBase, boolean lujo, String marca, int modelo, int nroLlantas) {
+		this.precioBase = precioBase;
+ 		this.lujo = lujo;
+ 		this.marca = marca;
+ 		this.modelo = modelo;
+ 		this.nroLlantas = nroLlantas;
+  }
+
+	// Los getters y setters de ser necesario
+
+	public double getPrecioBase() {
+  	return precioBase;
+  }
+  public String getMarca() {
+    return marca;
+  }
+  public boolean getLujo() {
+		return lujo;
+	}
+	public int getModelo() {
+		return modelo;
+	}
+	public int getNroLlantas() {
+		return nroLlantas;
+	}
+	// Resultados
+	public void setPrecioFinal(double precioFinal) {
+		this.precioFinal = precioFinal;
+	}
+	public double getPrecioFinal() {
+		return precioFinal;
+	}
+}
